6449bbfd5ee0f496806eaa9252f5f1e579f8a56e
Enrique Crespo <jecrespom@gmail.com>
2019-07-03 15:05:41 +0200

    coche fantastico serial

--- coche_fantastico_serial/coche_fantastico_serial.ino
+++ coche_fantastico_serial/coche_fantastico_serial.ino
@@ -5,25 +5,47 @@ int timer = 100;
 
 void setup() {
   // we make all the declarations at once
+  Serial.begin(9600);
   for (count = 0; count < 5; count++) {
     pinMode(pinArray[count], OUTPUT);
   }
 }
 
 void loop() {
-  //timer = analogRead(A0);  //El valor leido por analog read es el temporizador
+  timer = lee_serial();
+  Serial.println(timer);
   for (count = 0; count < 5; count++) {
-    //timer = analogRead(A0);
+    timer = lee_serial();
     digitalWrite(pinArray[count], HIGH);
     delay(timer);
     digitalWrite(pinArray[count], LOW);
     delay(timer);
   }
   for (count = 4; count >= 0; count--) {
-    //timer = analogRead(A0);
+    timer = lee_serial();
     digitalWrite(pinArray[count], HIGH);
     delay(timer);
     digitalWrite(pinArray[count], LOW);
     delay(timer);
   }
+}
+
+int lee_serial() {
+
+  if (Serial.available() > 0) {
+    Serial.println("leo...");
+    String leido = "";
+    do {
+      char caracter_leido;
+      delay(5);
+      caracter_leido = Serial.read();
+      leido += caracter_leido;
+    }  while (Serial.available() > 0);
+
+    return (leido.toInt());
+
+    Serial.println("He leido la cadena: \"" + leido + "\"");
+  }
+  else
+    return timer;
 }
